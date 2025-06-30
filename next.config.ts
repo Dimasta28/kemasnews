fb7c9bcfb2cc766a0136d51386e54e4ece5edf3f
Dimas <dimas@kemaspkg.com>
2025-06-30 07:26:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -25,6 +26,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'www.kemaspkg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ddujuwmrnfufdqnvgaqb.supabase.co',
         port: '',
         pathname: '/**',
       },
