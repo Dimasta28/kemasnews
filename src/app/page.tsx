8959fb50f6caf01cac3f9a75a0dd10eebf25cf58
Dimas <dimas@kemaspkg.com>
2025-06-30 04:13:20 +0000

    hapus metode login menggunakan google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,24 +35,6 @@ export default function LoginPage() {
           />
         </CardHeader>
         <CardContent className="grid gap-4">
-          <div className="grid grid-cols-1 gap-6">
-            <Button variant="outline">
-              <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-                <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 126 23.4 172.9 61.9l-69.8 69.8C314.1 101.3 282.8 88 248 88c-77.4 0-140.3 62.9-140.3 140.3s62.9 140.3 140.3 140.3c84.3 0 119.3-61.2 122.7-88.3h-122.7v-81.6h217.1c1.8 10.3 2.9 20.9 2.9 31.9z"></path>
-              </svg>
-              Google
-            </Button>
-          </div>
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                Or continue with
-              </span>
-            </div>
-          </div>
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input id="email" type="email" placeholder="m@example.com" required />
