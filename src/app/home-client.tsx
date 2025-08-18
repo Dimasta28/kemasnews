3a8d8df1f22528edb1e400139739ec5e59136c6f
Dimas <dimas@kemaspkg.com>
2025-08-18 04:38:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/home-client.tsx
+++ src/app/home-client.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
